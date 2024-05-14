9e34e0d05323a4da0187ca381496709afa341836
xulibang <xulibangyaojingjing@gmail.com>
2024-05-15 00:55:14 +0800

    feat: add model

--- src/renderer/src/components/Role.tsx
+++ src/renderer/src/components/Role.tsx
@@ -2,10 +2,10 @@ import { useEffect } from 'react'
 
 function Role(): JSX.Element {
   useEffect(() => {
-    loadlive2d('live2d', '/Resources/Hiyori/Hiyori.model3.json')
+    loadlive2d('live2d', '/model1/22/model.default.json')
   }, [])
 
-  return <canvas id="live2d" width="280" height="250"></canvas>
+  return <canvas className="live2d" id="live2d" width="280" height="250"></canvas>
 }
 
 export default Role
